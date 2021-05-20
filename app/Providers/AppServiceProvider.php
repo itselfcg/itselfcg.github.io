3a6244e537f2685bf0ac5b3dddac5ac21fa13b49
Itzel Contreras <itselfcg@gmail.com>
2021-05-20 14:28:49 -0700

    Fix: asset in https files.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Contracts\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,6 +28,6 @@ class AppServiceProvider extends ServiceProvider
     {
         if(env('REDIRECT_HTTPS')) {
             $this->app['request']->server->set('HTTPS', true);
-        }
+    }
     }
 }
